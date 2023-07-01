d13e4d882bac776df481c49e329b1f9e952d29d2
guymeiri <guymeiri@microsoft.com>
2023-07-01 14:55:28 +0300

    handle onChange for the upload

--- app/components/UploadModal.tsx
+++ app/components/UploadModal.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Modal from "./Modal";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      //todo: reset the form
+      uploadModal.onClose();
+    }
+  };
+
   return (
     <Modal
       title="Upload modal title"
       description="Upload modal description"
-      isOpen
-      onChange={() => {}}
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
     >
       Upload content
     </Modal>
